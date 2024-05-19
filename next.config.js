df175e5758aaf741de659e24068ce784d17aaa45
Alikhan Tortay <42945089+alikhantortay@users.noreply.github.com>
2024-05-19 21:24:32 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        loader: 'imgix',
+        path: '',
+        unoptimized: true
+    },
+    basePath: isProd ? '/qazaqstroy' : '',
+    assetPrefix: isProd ? '/qazaqstroy' : '',
     output: "export",
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
